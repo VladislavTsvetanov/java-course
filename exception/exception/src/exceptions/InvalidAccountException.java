3e84db27a8c9d5566d6f6caef3f318e4cb5d768d
VladislavTsvetanov <vladkogreen@gmail.com>
2016-01-17 18:28:03 +0200

    added exception homework

---
+++ exception/exception/src/exceptions/InvalidAccountException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class InvalidAccountException extends Exception {
+
+	private static final long serialVersionUID = 6792543878168050183L;
+
+	public InvalidAccountException() {
+		super();
+	}
+
+}
---
+++ exception/exception/src/exceptions/NegativeMoneyException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class NegativeMoneyException extends Exception {
+
+	private static final long serialVersionUID = -882090363926365908L;
+
+	public NegativeMoneyException(String message) {
+		super(message);
+
+	}
+
+}
